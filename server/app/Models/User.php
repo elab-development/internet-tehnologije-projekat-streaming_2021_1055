f519735249e6e6f13c7172e0cb61525fd320c5b3
MilovanMiletic <milovanmiletic230@gmail.com>
2024-01-22 21:14:00 +0100

    Izmenjene kolone useru

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -41,5 +42,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'admin' => 'boolean'
     ];
 }
